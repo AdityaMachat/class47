1375f2736b718d1471ebcab4eb35f0bf8aac0eb8
aditya <geethaaliyangal@gmail.com>
2020-09-19 16:55:25 +0530

    stage2

--- sketch.js
+++ sketch.js
@@ -1,73 +1,117 @@
-var ground,player;
-var w=800;
-var h=400;
-var roomw=w;
-var nextRoomReached=0;
+var ground, player;
+var w = 800;
+var h = 400;
+var roomw = w;
+var nextRoomReached = 0;
 var image1;
 var playerImage;
-var drawingRoom,bedroom,hall;
+var drawingRoom, bedroom, hall;
 var bg;
-var gameState="start";
+var gameState = "start";
 var choice = 0;
 var seconds;
+var optionA, optionB;
+var flag = 0;
 
-function preload(){
-  image1=loadImage("temporary.png");
-  playerImage=loadAnimation("images/1.png","images/2.png","images/3.png");
-  drawingRoom=loadImage("images/BACK1.jpg");
-  bedroom=loadImage("images/back3.jpg");
-  hall=loadImage("images/BACK2.jpg");
+function preload() {
+  image1 = loadImage("temporary.png");
+  playerImage = loadAnimation("images/1.png", "images/2.png", "images/3.png");
+  drawingRoom = loadImage("images/BACK1.jpg");
+  bedroom = loadImage("images/back3.jpg");
+  hall = loadImage("images/BACK2.jpg");
 }
 
 function setup() {
-  createCanvas(w,h);
+  createCanvas(w, h);
 
-  bg=createSprite(w/2,h/2,20,20);
-  bg.addImage("bedroom",bedroom);
-  bg.addImage("drawingroom",drawingRoom);
-  bg.addImage("Hall",hall);
+  bg = createSprite(w / 2, h / 2, 20, 20);
+  bg.addImage("bedroom", bedroom);
+  bg.addImage("drawingroom", drawingRoom);
+  bg.addImage("Hall", hall);
 
- // ground= createSprite(w/2, 0,w, h);
+  // ground= createSprite(w/2, 0,w, h);
   //ground.addImage(image1);
   //ground.scale=4;
 
-  player=createSprite(w/2,h/2,20,20);
-  player.addAnimation("playersprite",playerImage);
-  player.scale=0.5;
+  player = createSprite(w / 2, h - 50, 20, 20);
+  player.addAnimation("playersprite", playerImage);
+  player.scale = 1;
 
-  seconds=second();
+  seconds = second();
 
   console.log(seconds);
 }
 
 function draw() {
-  background(255);  
-  
+  background(255);
 
-  if(choice === 1){
-    bg.changeAnimation("drawingroom",drawingRoom);
-  }
-  else if(choice === 2){
-    bg.changeAnimation("Hall",hall);
-  }
-  
-  camera.position.x=player.x;
-  if(keyDown(RIGHT_ARROW)){
+
+
+
+  camera.position.x = player.x;
+  if (keyDown(RIGHT_ARROW)) {
     //need to slow down player speed
-    var currentPosition=player.x;
+    var currentPosition = player.x;
 
-    while(player.x!==currentPosition+roomw){
-    player.x=player.x+1;
+    while (player.x !== currentPosition + roomw) {
+      player.x = player.x + 1;
     }
   }
   drawSprites();
-  if(second()===(seconds+30)%60 && gameState==="start"){
-    gameState="play";
+  if (flag !== 0 && gameState === "start") {
+    gameState = "play";
     console.log("hello");
-  }else if(gameState!=="play"){
-   textSize(25);
+  } else if (gameState !== "play") {
+    textSize(15);
     fill("black");
-    stroke("grren")
-    text("intro",200,50);
+    stroke("white")
+    text("You wake up in a room which you do not recognise and you hear someone ", 200, 20);
+    text("calling out your enemy's name,there are two doors A and B.  ", 200, 40);
+    text("Now you have to decide which room you want to go to. ", 200, 60);
+    optionA = createSprite(300, 200, 80, 50);
+    optionB = createSprite(500, 200, 80, 50);
+    text("Option A", 270, 200);
+    text("Option B", 470, 200);
+    if (mousePressedOver(optionA)) {
+      flag = 1;
+      bg.changeAnimation("drawingroom", drawingRoom);
+
+    } else if (mousePressedOver(optionB)) {
+      flag = 2;
+      bg.changeAnimation("Hall", hall);
+    }
+
+  }
+
+  if (flag === 1) {
+    text("intro", 200, 50);
+    if (mousePressedOver(next)) {
+      text("intro", 200, 50);
+      text("Option A", 270, 200);
+      text("Option B", 470, 200);
+      if (mousePressedOver(optionA)) {
+        flag = 3;
+
+      } else if (mousePressedOver(optionB)) {
+        flag = 4;
+
+      }
+    }
+
+    if (flag === 2) {
+      text("intro", 200, 50);
+      if (mousePressedOver(next)) {
+        text("intro", 200, 80);
+        text("Option A", 270, 200);
+        text("Option B", 470, 200);
+        if (mousePressedOver(optionA)) {
+          flag = 5;
+
+        } else if (mousePressedOver(optionB)) {
+          flag = 6;
+
+        }
+      }
+    }
   }
 }
